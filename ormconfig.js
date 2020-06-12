a6e1ddb73d42400eab3ad25c182de036751fb26d
Kioketsuky <davideduardomaradiaga@gmail.com>
2020-06-12 00:01:57 -0600

    Ötro cambion en env

---
+++ ormconfig.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+module.exports = {
+	type: "postgres",
+	host: process.env.TYPEORM_HOST,
+	port: parseInt(process.env.TYPEORM_PORT),
+	username: process.env.TYPEORM_USERNAME,
+	password: process.env.TYPEORM_PASSWORD,
+	database: process.env.TYPEORM_DATABASE,
+    entities: "dist/**/*.entity{.ts,.js}"
+};

713f14e147dc27069b83e3acbb3af18e61d7119d
Kioketsuky <davideduardomaradiaga@gmail.com>
2020-06-17 22:51:49 -0600

    .env arreglado

--- ormconfig.js
+++ ormconfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    type: process.env.TYPEORM_CONNECTION,
+    type: process.env.TYPEORM_CONEC,
     host: process.env.TYPEORM_HOST,
     port: parseInt(process.env.TYPEORM_PORT),
     username: process.env.TYPEORM_USERNAME,
